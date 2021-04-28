af2dc9f3a2302f45baf808fae65106952802761e
Shira-Meirovitz <shirameirovi@gmail.com>
2021-04-28 11:59:30 +0300

    added all options to app

--- app.js
+++ app.js
@@ -1,12 +1,80 @@
-/* write your server code here */
 const express = require('express');
-const api = require('./router/api');
-const users = require('./router/users');
+const api = require('./routes/api');
+const users = require('./routes/users');
+const jwt = require('jsonwebtoken');
+const { ACCESS_TOKEN_SECRET } = require('./env');
 
 const app = express();
+
 app.use(express.json());
 
 app.use('/api/v1', api);
 app.use('/users', users);
+
+app.options('/', (req, res) => {
+  const allOptions = [
+    {
+      method: 'post',
+      path: '/users/register',
+      description: 'Register, Required: email, user, password',
+      example: {
+        body: { email: 'user@email.com', name: 'user', password: 'password' },
+      },
+    },
+    {
+      method: 'post',
+      path: '/users/login',
+      description: 'Login, Required: valid email and password',
+      example: { body: { email: 'user@email.com', password: 'password' } },
+    },
+    {
+      method: 'post',
+      path: '/users/token',
+      description: 'Renew access token, Required: valid refresh token',
+      example: { headers: { token: '*Refresh Token*' } },
+    },
+    {
+      method: 'post',
+      path: '/users/tokenValidate',
+      description: 'Access Token Validation, Required: valid access token',
+      example: { headers: { authorization: 'Bearer *Access Token*' } },
+    },
+    {
+      method: 'get',
+      path: '/api/v1/information',
+      description: "Access user's information, Required: valid access token",
+      example: { headers: { authorization: 'Bearer *Access Token*' } },
+    },
+    {
+      method: 'post',
+      path: '/users/logout',
+      description: 'Logout, Required: access token',
+      example: { body: { token: '*Refresh Token*' } },
+    },
+    {
+      method: 'get',
+      path: 'api/v1/users',
+      description: 'Get users DB, Required: Valid access token of admin user',
+      example: { headers: { authorization: 'Bearer *Access Token*' } },
+    },
+  ];
+  const authHeader = req.headers['authorization'];
+
+  const token = authHeader && authHeader.slice(7);
+  let returnedOptions = [];
+  let returnedIndexes = [0, 1];
+
+  if (token) {
+    returnedIndexes.push(2);
+    jwt.verify(token, ACCESS_TOKEN_SECRET, (err, data) => {
+      if (err) return;
+      returnedIndexes.push(3, 4, 5);
+      if (data.isAdmin) returnedIndexes.push(6);
+    });
+  }
+  returnedOptions = allOptions.filter((op, i) => returnedIndexes.includes(i));
+
+  res.set('Allow', 'OPTIONS, GET, POST').status(200).json(returnedOptions);
+});
 
 module.exports = app;
