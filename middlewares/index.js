b1ab3f8effa727499ab0112e960ffc7c98e5b76a
Shira-Meirovitz <shirameirovi@gmail.com>
2021-04-28 11:56:58 +0300

    fixed users code

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,19 +1,21 @@
-const jwt = require('jsonwebtoken');
 const { ACCESS_TOKEN_SECRET } = require('../env');
+const jwt = require('jsonwebtoken');
 
 function validateToken(req, res, next) {
   const authHeader = req.headers['Authorization'];
   const token = authHeader && authHeader.slice(7);
+
   if (!token) {
     return res.status(401).send('Access Token Required');
   }
+
   jwt.verify(token, ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      console.log(err);
-      return res.status(403).send('Invalid Access token');
+      return res.status(403).send('Invalid Access Token');
     }
     req.user = decoded;
     next();
   });
 }
+
 module.exports = { validateToken };
--- router/users.js
+++ router/users.js
@@ -1,68 +1,81 @@
 const { hashSync, compare } = require('bcrypt');
 const { Router } = require('express');
-const users = Router();
+const { hashSync, compare } = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { Router } = require('express');
 const { USERS, INFORMATION, REFRESHTOKENS } = require('../helpers');
 const { REFRESH_TOKEN_SECRET, ACCESS_TOKEN_SECRET } = require('../env');
 const { validateToken } = require('../middlewares');
 
+const users = Router();
+
 users.post('/register', (req, res) => {
   const { email, name, password } = req.body;
 
-  //check if user exists
+  // Check if user exists
   const checkUser = USERS.find((user) => email === user.email);
-  //if user exists, send appropriate response
+
+  // If user exists, send appropriate response
   if (checkUser) {
     return res.status(409).send('user already exists');
   }
+
+  // If user does not exist, create it:
+  // Hash password
   const hashedPassword = hashSync(password, 10);
 
-  //Adding user to DB
+  // Adding user to 'DB'
   USERS.push({
     email,
     name,
     password: hashedPassword,
     isAdmin: false,
   });
-  //Adding information to DB
+
+  // Adding information to 'DB'
   INFORMATION.push({
     email,
     info: `${name} info`,
   });
-  res.status(201).send('Register success');
+
+  res.status(201).send('Register Success');
 });
 
-users.post('./login', async (req, res) => {
+users.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   const user = USERS.find((entry) => entry.email === email);
 
   if (!user) {
-    return res.status(404).send('Cannot find user');
+    return res.status(404).send('cannot find user');
   }
+
   try {
     const isPasswordCorrect = await compare(password, user.password);
     if (!isPasswordCorrect) {
-      return res.status(403).send('User orPassword incorrect');
+      return res.status(403).send('User or Password incorrect');
     }
+
     const dataInToken = {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
     };
+
     const refreshToken = jwt.sign(dataInToken, REFRESH_TOKEN_SECRET);
     const accessToken = jwt.sign(dataInToken, ACCESS_TOKEN_SECRET, {
       expiresIn: '10s',
     });
 
     REFRESHTOKENS.push(refreshToken);
+
     res.json({
       accessToken,
       refreshToken,
       ...dataInToken,
     });
-  } catch {
-    console.log(err);
+  } catch (error) {
+    console.log(error);
     res.sendStatus(500);
   }
 });
@@ -75,34 +88,48 @@ users.post('/token', (req, res) => {
   const { token } = req.body;
 
   if (!token) {
-    res.status(401).send('Refresh Token Required');
+    return res.status(401).send('Refresh Token Required');
   }
+
   if (!REFRESHTOKENS.includes(token)) {
-    return res.json(403).send('Invalid Refresh Token');
+    return res.status(403).send('Invalid Refresh Token');
   }
+
   jwt.verify(token, REFRESH_TOKEN_SECRET, (err, decoded) => {
     if (err) {
       return res.status(403).send('Invalid Refresh Token');
     }
-    const accessToken = jwt.sign(decoded, ACCESS_TOKEN_SECRET, {
+    const user = {
+      name: decoded.name,
+      email: decoded.email,
+      isAdmin: decoded.isAdmin,
+    };
+    const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET, {
       expiresIn: '10s',
     });
+
     return res.json({ accessToken });
   });
 });
 
 users.post('/logout', (req, res) => {
   const { token } = req.body;
+
   if (!token) {
     return res.status(400).send('Refresh Token Required');
   }
+
   const refreshTokenIndex = REFRESHTOKENS.findIndex(
     (rToken) => rToken === token
   );
+
   if (refreshTokenIndex === -1) {
     return res.status(400).send('Invalid Refresh Token');
   }
+
   REFRESHTOKENS.splice(refreshTokenIndex, 1);
-  return res.send('UserLogged Out Successfully');
+
+  return res.send('User Logged Out Successfully');
 });
+
 module.exports = users;
